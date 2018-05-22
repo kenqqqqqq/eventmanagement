cc910967c1695e1c2def2513c46575a4547460cd
chester <ckraigpc@gmail.com>
2018-05-22 10:39:56 -0700

    Files for the event app management

---
+++ app.js
@@ -0,0 +1,92 @@
+/*1.**/
+// Create a variable the will hold form elements
+ 
+// input elements
+ 
+// createLI function
+ 
+  //create a UI element to append value in a list item
+   
+  // assign text value on the list item
+ 
+  // create a UI element label for the checkbox
+   
+  // add a textcontent on the label "confirm"
+ 
+  // create a UI checkbox and store the label on it
+   
+  // and append these elements in the list items
+ 
+  // create a UI remove button and store the label on it
+   
+  /**5 */
+  // create a UI edit button and store the label on it 
+  // now once test the edit and remove button both delete the invitees
+  // modify the event made for the removing elements below
+  
+// Add a submit event handler to the form
+ 
+  // create a handler to hold the names and select the element UI "ul"
+ 
+  // // create a UI element to append value in a list item
+ 
+
+  // // assign text value on the list item
+ 
+
+  // // create a UI element label for the checkbox
+ 
+  // // add a textcontent on the label "confirm"
+ 
+
+  // // create a UI checkbox and store the label on it
+
+
+  // create a UI remove button and store the label on it
+ 
+ 
+  // append listitem on the ul element
+ 
+
+  // clear input value
+ 
+ 
+/*2**/
+// add an event Listener event object "change" only for the parent element of li,checbox,label
+// use an event object on the function parameter
+ 
+  // create a handler for checkbox event target
+ 
+  // create a hanlder for checkbox value if checked
+ 
+  // create a handler that will traverse or call the parent elements of checkbock li
+ 
+  // create a condition to change the event wether a checbox is checked or not
+  // but before creating this check the classname "responded" in the style.css
+ 
+      // add the class "responded" on the list item parent
+    
+    // add a class of empty string
+     
+/*3**/
+  // add eventlistener event object "click" for the button remove but target the ul
+  // use an event object on the function parameter
+   
+    //create a handler to target the parent of the target which is the li
+     
+    //create a handler also for the main parent of the target which is the ul
+  
+    // create a condition only select "button" once triggered the event for the ul  
+     
+          // remove the child li from the ul
+          
+/*4**/
+// Refractor the codefirst , check the submit handler on the top.. think of how you can
+// break the code and simplify it store these resuable code in a function
+
+
+
+/*6**/
+// At this point you will have to work with the edit state,saving name,fiter invitees who have not responded
+// no comment will b provided
+// 
